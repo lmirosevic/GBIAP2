4a2bfbd7baf076773856517f664d89edfc9cab1b
Luka Mirosevic <luka@goonbee.com>
2013-05-20 09:56:01 +0200

    Initial Commit

---
+++ GBIAP2/GBIAP2.h
@@ -0,0 +1,13 @@
+//
+//  GBIAP2.h
+//  GBIAP2
+//
+//  Created by Luka Mirosevic on 20/05/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GBIAP2 : NSObject
+
+@end
---
+++ GBIAP2/GBIAP2.m
@@ -0,0 +1,13 @@
+//
+//  GBIAP2.m
+//  GBIAP2
+//
+//  Created by Luka Mirosevic on 20/05/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import "GBIAP2.h"
+
+@implementation GBIAP2
+
+@end
